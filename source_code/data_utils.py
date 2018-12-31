7f48b234748815d4ad3517eece959cfe10dea6fa
Uzair Akbar <uzairakbar@outlook.com>
2018-12-31 17:18:38 +0100

    Update data_utils.py

--- source_code/data_utils.py
+++ source_code/data_utils.py
@@ -18,31 +18,57 @@ import matplotlib.image as mpimg
 ########### TEMP #################
 
 # pylint: disable=C0326
+# SEG_LABELS_LIST = [
+#     {"id": 23, "name": "void",       "rgb_values": [0,   0,    0]},
+#     {"id": 0,  "name": "building",   "rgb_values": [128, 0,    0]},
+#     {"id": 1,  "name": "grass",      "rgb_values": [0,   128,  0]},
+#     {"id": 2,  "name": "tree",       "rgb_values": [128, 128,  0]},
+#     {"id": 3,  "name": "cow",        "rgb_values": [0,   0,    128]},
+#     {"id": 4,  "name": "horse",      "rgb_values": [128, 0,    128]},
+#     {"id": 5,  "name": "sheep",      "rgb_values": [0,   128,  128]},
+#     {"id": 6,  "name": "sky",        "rgb_values": [128, 128,  128]},
+#     {"id": 7,  "name": "mountain",   "rgb_values": [64,  0,    0]},
+#     {"id": 8,  "name": "airplane",   "rgb_values": [192, 0,    0]},
+#     {"id": 9,  "name": "water",      "rgb_values": [64,  128,  0]},
+#     {"id": 10, "name": "face",       "rgb_values": [192, 128,  0]},
+#     {"id": 11, "name": "car",        "rgb_values": [64,  0,    128]},
+#     {"id": 12, "name": "bicycle",    "rgb_values": [192, 0,    128]},
+#     {"id": 13, "name": "flower",     "rgb_values": [64,  128,  128]},
+#     {"id": 14, "name": "sign",       "rgb_values": [192, 128,  128]},
+#     {"id": 15, "name": "bird",       "rgb_values": [0,   64,   0]},
+#     {"id": 16, "name": "book",       "rgb_values": [128, 64,   0]},
+#     {"id": 17, "name": "chair",      "rgb_values": [0,   192,  0]},
+#     {"id": 18, "name": "road",       "rgb_values": [128, 64,   128]},
+#     {"id": 19, "name": "cat",        "rgb_values": [0,   192,  128]},
+#     {"id": 20, "name": "dog",        "rgb_values": [128, 192,  128]},
+#     {"id": 21, "name": "body",       "rgb_values": [64,  64,   0]},
+#     {"id": 22, "name": "boat",       "rgb_values": [192, 64,   0]}]
+
 SEG_LABELS_LIST = [
-    {"id": 23, "name": "void",       "rgb_values": [0,   0,    0]},
-    {"id": 0,  "name": "building",   "rgb_values": [128, 0,    0]},
-    {"id": 1,  "name": "grass",      "rgb_values": [0,   128,  0]},
-    {"id": 2,  "name": "tree",       "rgb_values": [128, 128,  0]},
-    {"id": 3,  "name": "cow",        "rgb_values": [0,   0,    128]},
-    {"id": 4,  "name": "horse",      "rgb_values": [128, 0,    128]},
-    {"id": 5,  "name": "sheep",      "rgb_values": [0,   128,  128]},
-    {"id": 6,  "name": "sky",        "rgb_values": [128, 128,  128]},
-    {"id": 7,  "name": "mountain",   "rgb_values": [64,  0,    0]},
-    {"id": 8,  "name": "airplane",   "rgb_values": [192, 0,    0]},
-    {"id": 9,  "name": "water",      "rgb_values": [64,  128,  0]},
-    {"id": 10, "name": "face",       "rgb_values": [192, 128,  0]},
-    {"id": 11, "name": "car",        "rgb_values": [64,  0,    128]},
-    {"id": 12, "name": "bicycle",    "rgb_values": [192, 0,    128]},
-    {"id": 13, "name": "flower",     "rgb_values": [64,  128,  128]},
-    {"id": 14, "name": "sign",       "rgb_values": [192, 128,  128]},
-    {"id": 15, "name": "bird",       "rgb_values": [0,   64,   0]},
-    {"id": 16, "name": "book",       "rgb_values": [128, 64,   0]},
-    {"id": 17, "name": "chair",      "rgb_values": [0,   192,  0]},
-    {"id": 18, "name": "road",       "rgb_values": [128, 64,   128]},
-    {"id": 19, "name": "cat",        "rgb_values": [0,   192,  128]},
-    {"id": 20, "name": "dog",        "rgb_values": [128, 192,  128]},
-    {"id": 21, "name": "body",       "rgb_values": [64,  64,   0]},
-    {"id": 22, "name": "boat",       "rgb_values": [192, 64,   0]}]
+    {"id": 0, "name": "void",       "rgb_values": [0,   0,    0]},
+    {"id": 1,  "name": "building",   "rgb_values": [128, 0,    0]},
+    {"id": 2,  "name": "grass",      "rgb_values": [0,   128,  0]},
+    {"id": 3,  "name": "tree",       "rgb_values": [128, 128,  0]},
+    {"id": 4,  "name": "cow",        "rgb_values": [0,   0,    128]},
+    {"id": 5,  "name": "horse",      "rgb_values": [128, 0,    128]},
+    {"id": 6,  "name": "sheep",      "rgb_values": [0,   128,  128]},
+    {"id": 7,  "name": "sky",        "rgb_values": [128, 128,  128]},
+    {"id": 8,  "name": "mountain",   "rgb_values": [64,  0,    0]},
+    {"id": 9,  "name": "airplane",   "rgb_values": [192, 0,    0]},
+    {"id": 10,  "name": "water",      "rgb_values": [64,  128,  0]},
+    {"id": 11, "name": "face",       "rgb_values": [192, 128,  0]},
+    {"id": 12, "name": "car",        "rgb_values": [64,  0,    128]},
+    {"id": 13, "name": "bicycle",    "rgb_values": [192, 0,    128]},
+    {"id": 14, "name": "flower",     "rgb_values": [64,  128,  128]},
+    {"id": 15, "name": "sign",       "rgb_values": [192, 128,  128]},
+    {"id": 16, "name": "bird",       "rgb_values": [0,   64,   0]},
+    {"id": 17, "name": "book",       "rgb_values": [128, 64,   0]},
+    {"id": 18, "name": "chair",      "rgb_values": [0,   192,  0]},
+    {"id": 19, "name": "road",       "rgb_values": [128, 64,   128]},
+    {"id": 20, "name": "cat",        "rgb_values": [0,   192,  128]},
+    {"id": 21, "name": "dog",        "rgb_values": [128, 192,  128]},
+    {"id": 22, "name": "body",       "rgb_values": [64,  64,   0]},
+    {"id": 23, "name": "boat",       "rgb_values": [192, 64,   0]}]
 
 
 def label_img_to_rgb(label_img):
@@ -183,8 +209,8 @@ class SegmentationData(data.Dataset):
         print("original shape", target.shape)
         if target.shape[1] <= 10:
             print("original", target)
-        print("original buildings", np.sum(np.all(target == np.array([[[128]], [[0]], [[0]]]), axis=0)))
-        print("original sign", np.sum(np.all(target == np.array([[[192]], [[128]], [[128]]]), axis=0)))
+        print("original buildings", np.sum(np.all(target == [128, 0, 0], axis=0)))
+        print("original sign", np.sum(np.all(target == [192, 128, 128], axis=0)))
         
         tl = target
         target_labels = target[..., 0]
